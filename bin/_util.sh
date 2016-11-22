16646a2f4f718384d8ea7b35a3e8fe4759c3967d
Paul Vollmer <paul.vollmer@protonmail.com>
2016-11-22 08:56:49 +0100

    DRY cleanup at bin scripts

---
+++ bin/_util.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+
+echo "BIN UTIL..."
+
+SCRIPT="$0"
+
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi
+done
+
+if [ ! -d "${APP_DIR}" ]; then
+  APP_DIR=`dirname "$SCRIPT"`/..
+  APP_DIR=`cd "${APP_DIR}"; pwd`
+fi
+
+executable="./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
+
+if [ ! -f "$executable" ]
+then
+  mvn clean package
+fi
--- bin/go-petstore-server.sh
+++ bin/go-petstore-server.sh
@@ -1,31 +1,7 @@
 #!/bin/sh
-
-SCRIPT="$0"
-
-while [ -h "$SCRIPT" ] ; do
-  ls=`ls -ld "$SCRIPT"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '/.*' > /dev/null; then
-    SCRIPT="$link"
-  else
-    SCRIPT=`dirname "$SCRIPT"`/"$link"
-  fi
-done
-
-if [ ! -d "${APP_DIR}" ]; then
-  APP_DIR=`dirname "$SCRIPT"`/..
-  APP_DIR=`cd "${APP_DIR}"; pwd`
-fi
-
-executable="./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
-
-if [ ! -f "$executable" ]
-then
-  mvn clean package
-fi
+source bin/_util.sh
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
 ags="$@ generate -i modules/swagger-codegen/src/test/resources/2_0/petstore.yaml -l go-server -o samples/server/petstore/go-api-server -DpackageName=petstoreserver "
-
 java $JAVA_OPTS -Dservice -jar $executable $ags
